b3134da0fc1020a92089247984d1a7f4297b0209
jtagoeASC <tagoej@bxscience.edu>
2016-08-18 15:18:53 -0400

    Website

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$('#mainButton').click(function(){
+		$('#mainButton').html('<A HREF="https://github.com/jtagoeASC">Github Account</A>');
+
+	});
+});
+
+$(document).ready(function(){
+	$('#mainButton2').click(function(){
+		$('#mainButton2').html('<A HREF="https://nba.com">NBA Website</A>');
+
+	});
+});
